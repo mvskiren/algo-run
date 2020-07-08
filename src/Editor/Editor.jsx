7e04805993c15fd490b94e84b66d50322a71647a
MvsKiran <kiranmvs07@gmail.com>
2020-07-08 18:59:57 +0530

    Updated required elements
    Input and output Textarea, Run button, Select dropdown

--- src/Editor/Editor.jsx
+++ src/Editor/Editor.jsx
@@ -3,7 +3,8 @@ import { Controlled as CodeMirror } from "react-codemirror2";
 import "codemirror/lib/codemirror.css";
 import "codemirror/theme/material.css";
 import "codemirror/mode/clike/clike";
-import 'codemirror/keymap/sublime'
+import 'codemirror/keymap/sublime';
+import classes from './style.module.css';
 
 class Editor extends Component {
   constructor() {
@@ -34,17 +35,37 @@ class Editor extends Component {
   };
 
   render() {
-    
+
     return (
-      <div style={{width:'75%'}}>
-        <CodeMirror
+      <div style={{width:'75%',margin:'4%'}}>
+      <label>Select language</label>
+       <select className={classes.dropdown}>
+         <option value="volvo">C++</option>
+         <option value="volvo">JAVA</option>
+         <option value="volvo">PYTHON</option>
+         <option value="volvo">JAVASCRIPT</option>
+         <option value="volvo">GO</option>
+        </select>
+        <CodeMirror 
+         className={classes.codemirror}
           value={this.state.value}
           options={{...this.state.options}}
           onBeforeChange={this.onBeforeChange}
           onChange={this.onChange}
         />
-        <p></p>
-        <button onClick={() => this.onSubmit()}>Submit</button>
+        
+        <button  type="submit" className={classes.hello} onClick={() => this.onSubmit()}>Run</button>
+        <label class="container">Custom Input
+            <input type="checkbox" />
+        </label>
+          <div style={{margin:'2%',display:'flex','flex-direction': 'row',justifyContent:'center'}}>
+            <div style={{margin: '5%'}}>
+            <textarea value='Input....' rows="10" cols="30"/>
+            </div>
+            <div style={{margin: '5%'}}>
+            <textarea value='Output from server...' rows="10" cols="50"/>
+            </div>
+          </div>
       </div>
     );
   }
